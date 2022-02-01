dbaa662f7260d4486d8d719f43ef925256bea496
Hitesh1436 <hsc253545@gmail.com>
2022-02-01 22:46:02 +0530

    added

--- 5_Js Concepts/Higher Order Functions/map.js
+++ 5_Js Concepts/Higher Order Functions/map.js
@@ -18,12 +18,38 @@ let sqarr = arr.map(function squarer(v){
 // console.log(arr);
 
 
-let nameArr = ['Hitesh','Mrinal','Tania'];
+// let nameArr = ['Hitesh','Mrinal','Tania'];
 
-// print names using the map function
+// // print names using the map function
 
-let printArr = nameArr.map(function name(n){
-     return n;
+// let printArr = nameArr.map(function name(n){
+//      return n;
+// })
+
+// console.log(printArr)
+
+
+let nameArr = ['Vishal Kumar' , 'Vaibahv Rawal' , 'Anmol Taneja'];
+
+
+// You have to use map function and you will take out firstName and lastName separately
+
+let modifiedArray = nameArr.map(function(n){
+    let partOfName = n.split(' ');
+    return partOfName;
 })
 
-console.log(printArr)
+// console.log(modifiedArray)
+
+
+const transactions = [1000, 3000, 4000, 2000, -898, 3800, -4500];  // dollar mn h mnlo ab Rs mn convert krrhe hn bas 
+const inrtToUsd = 74;
+
+
+let transactioninRs = transactions.map(function(n){
+    return n*inrtToUsd;
+})
+
+console.log(transactions)
+
+console.log(transactioninRs)
