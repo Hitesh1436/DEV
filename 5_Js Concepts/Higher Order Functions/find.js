c0048023b11a2bbfb28938896656d7fac8a71d60
Hitesh1436 <hsc253545@gmail.com>
2022-02-01 23:52:13 +0530

    added

---
+++ 5_Js Concepts/Higher Order Functions/find.js
@@ -0,0 +1,9 @@
+// Find returns the first element of an array that satisfies the condition
+
+const transactions = [1000, 3000, 4000, 2000, -898, 3800, -4500];
+
+const firstWithdrawal = transactions.find(function(n){
+    return n<0;
+})
+
+console.log(firstWithdrawal)
