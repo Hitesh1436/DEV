2e05b71c0ecb14bdb905949c97b8c4dab08b62de
Hitesh1436 <hsc253545@gmail.com>
2022-02-04 11:16:29 +0530

    added

--- 7_webScraping/ESPNscrapper/main.js
+++ 7_webScraping/ESPNscrapper/main.js
@@ -25,4 +25,32 @@ function extractLink(html){
     let fullLink = 'https://www.espncricinfo.com' + link;  // taki puri link show ho and uspr jaskee
     console.log(fullLink);
 
+
+    getAllMatchLink(fullLink);
 }
+
+function getAllMatchLink(uri){
+    request(uri , function(error,response,html){
+        if(error){
+            console.log(error);
+        }
+        else{
+            extractAllLink(html);
+        }
+    })
+}
+
+function extractAllLink(html){
+    let $ = cheerio.load(html);
+
+
+    let scoreCardArr = $('a[data-hover="Scorecard"]')
+
+    for(let i=0 ; i<scoreCardArr.length ;i++){
+        let link = $(scoreCardArr[i]).attr('href');
+        let fullLink = 'https://www.espncricinfo.com/' + link;
+        console.log(fullLink)
+    }
+}
+
+
---
+++ 7_webScraping/ESPNscrapper/scorecard.js
@@ -0,0 +1,36 @@
+let url = 'https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard';
+
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+
+request(url , function(error,response,html){
+    if(error){
+        console.error(error)
+    }
+    else{
+        extractMatchDetails(html)
+    }
+})
+
+function extractMatchDetails(html){
+    let $ = cheerio.load(html);
+
+    let descString = $('.header-info .description');
+
+    // console.log(descString)
+
+    let descStringArr = descString.text().split(',');
+
+    let venue = descStringArr[1].trim();
+    let date  = descStringArr[2].trim();
+
+    let result = $('.match-info.match-info-MATCH.match-info-MATCH-half-width .status-text span').text();
+       
+
+
+    console.log(venue);
+    console.log(date);
+    console.log(result);
+}
