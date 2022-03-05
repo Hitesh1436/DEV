335ee9bac86eef8460f26df237db74931f5504c3
Hitesh1436 <hsc253545@gmail.com>
2022-03-05 22:00:05 +0530

    mini

---
+++ 10_mniProjectPromises/demo.js
@@ -0,0 +1,16 @@
+var inquirer = require("inquirer")
+
+inquirer.prompt([       // question object h prompt 
+    {
+        type : 'list', 
+        name : 'selection',
+        choices : ['choice 1','choice 2']
+    }
+]).then(function(ans){
+    if(ans.selection == 'choice 1'){
+        console.log('Choice 1 is Selected')
+    }else{
+        console.log('Choice 2 is Selected')
+    }
+})
+
