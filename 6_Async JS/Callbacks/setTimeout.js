86724519888d21c9d0c7829fc614fdf466009ef6
Hitesh1436 <hsc253545@gmail.com>
2022-03-14 09:33:37 +0530

    setTimeout

--- 6_Async JS/Callbacks/setTimeout.js
+++ 6_Async JS/Callbacks/setTimeout.js
@@ -1,20 +1,21 @@
-// function greeting(name, role) {
-//     console.log(`Hello my name is ${name}`);
-//     console.log(`I'm a ${role}`);
-//   }
+// setTimeout is an Async function 
+function greeting(name, role) {
+    console.log(`Hello my name is ${name}`);   //``-> isse khte hn template literal tki string concat ni krni pde 
+    console.log(`I'm a ${role}`);
+  }
 
-//   setTimeout(greeting, 3000, "Nathan", "Software developer");
+  setTimeout(greeting, 3000, "Nathan", "Software developer");
 
 
 // SetTimeout  kse work krta h actual mn yh ek Async functiuon h toh same CallBack queue Node API callback stack vala same trika apply hoga isme bhi nd ussi ki vjh se 5 sec delay krrha h
 
-setTimeout(function(firstName){
+setTimeout(function (firstName) {
     console.log(firstName)
-},5000,'Hitesh')    // 5sec ko hum milliseconds mn dlte hn iss function mein
+}, 4000, 'Hitesh')    // 5sec ko hum milliseconds mn dlte hn iss function mein
 
+// upr 2 setTimeout use kia h toh uska mtlb yh ni ki 3 sec fir 3 sec delay agr niche 4000 krdunga toh upr vala  sec mn ayga nd niche vala uske 1 sec baad ayga console pr
 
-
-// SetInterval dkhenge ab 
+// SetInterval dkhenge ab
 
 // let count = 0;
 
